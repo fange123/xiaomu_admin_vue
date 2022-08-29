8cf3905c7b8a5b0c71e7522d72655a6603a164a3
fange123 <2030564240@qq.com>
2022-08-29 14:17:49 +0800

    feat:路由精简

--- src/utils/get-page-title.js
+++ src/utils/get-page-title.js
@@ -1,10 +1,10 @@
-import defaultSettings from "@/settings";
+import defaultSettings from '@/settings'
 
-const title = defaultSettings.title || "小木读书";
+const title = defaultSettings.title || '小木读书'
 
 export default function getPageTitle(pageTitle) {
   if (pageTitle) {
-    return `${pageTitle} - ${title}`;
+    return `${pageTitle} - ${title}`
   }
-  return `${title}`;
+  return `${title}`
 }
---
+++ src/views/create/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div />
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data() {
+    return {
+
+    }
+  },
+  created() {
+
+  },
+  // 生命周期 - 挂载完成（访问DOM元素）
+  mounted() {
+
+  }
+}
+</script>
+<style lang="less" scoped>
+
+</style>
