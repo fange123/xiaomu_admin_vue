da4d1e40e4c51c4fa4e48aab4bb94cd90c9694ab
fange123 <2030564240@qq.com>
2022-09-19 22:28:56 +0800

    fix:根据需求修改响应内容

--- src/utils/request.js
+++ src/utils/request.js
@@ -16,10 +16,8 @@ service.interceptors.request.use(
     // do something before request is sent
 
     if (store.getters.token) {
-      // let each request carry token
-      // ['X-Token'] is a custom headers key
-      // please modify it according to the actual situation
-      config.headers['X-Token'] = getToken()
+     // * Authorization:Bearer token  要符合jwt规范
+      config.headers['Authorization'] = `Bearer ${getToken()}`
     }
     return config
   },
@@ -32,23 +30,14 @@ service.interceptors.request.use(
 
 // response interceptor
 service.interceptors.response.use(
-  /**
-   * If you want to get http information such as headers or status
-   * Please return  response => response
-  */
-
-  /**
-   * Determine the request status by custom code
-   * Here is just an example
-   * You can also judge the status by HTTP Status Code
-   */
   response => {
     const res = response.data
+    const errMsg = res.msg || '请求失败'
 
     // if the custom code is not 20000, it is judged as an error.
-    if (res.code !== 20000) {
+    if (res.code !== 0) {
       Message({
-        message: res.message || 'Error',
+        message: errMsg,
         type: 'error',
         duration: 5 * 1000
       })
@@ -66,15 +55,15 @@ service.interceptors.response.use(
           })
         })
       }
-      return Promise.reject(new Error(res.message || 'Error'))
+      return Promise.reject(new Error(errMsg))
     } else {
       return res
     }
   },
   error => {
-    console.log('err' + error) // for debug
+    const {msg} = error.response.data
     Message({
-      message: error.message,
+      message: msg,
       type: 'error',
       duration: 5 * 1000
     })
