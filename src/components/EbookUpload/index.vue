ee04013e4cd1a364617c236375b1eb5509c423cb
fange123 <2030564240@qq.com>
2022-10-08 16:27:36 +0800

    feat:电子书上传组件开发

--- src/components/EbookUpload/index.vue
+++ src/components/EbookUpload/index.vue
@@ -1,7 +1,27 @@
 <!--  -->
 <template>
   <div class="upload-container">
-    <el-upload :action="action" :headers="headers">请上传</el-upload>
+    <el-upload
+     :action="action"
+     :headers="headers"
+     :multiple="false"
+     :limit="1"
+     :before-upload="beforeUpload"
+     :on-success="onSuccess"
+     :on-error="onError"
+     :on-remove="onRemove"
+     :file-list="fileList"
+     :on-exceed="onExceed"
+     :disabled="disabled"
+     drag
+     show-file-list
+     accept="application/epub+zip"
+     class="image-upload"
+     >
+      <i class="el-icon-upload"></i>
+      <div class="el-upload__text" v-if="!fileList.length">请将电子书拖入或<em>点击上传</em></div>
+      <div class="el-upload__text" v-else>电子书已上传</div>
+    </el-upload>
 
   </div>
 </template>
@@ -12,7 +32,9 @@ export default {
     name:'index',
     data() {
         return {
-          action:`${process.env.VUE_APP_BASE_API}/book/upload`,
+          action:`${process.env.VUE_APP_BASE_API}/api/user/book/upload`,
+          fileList:[],
+          disabled:false
         }
     },
     computed: {
@@ -29,6 +51,28 @@ export default {
     //生命周期 - 挂载完成（访问DOM元素）
     mounted() {
 
+    },
+    methods: {
+      beforeUpload(file){
+        console.log(file);
+        this.$emit('beforeUpload',file);
+      },
+      onSuccess(){},
+      onError(err){
+      const errorMsg = JSON.parse(err.message)
+        this.$message({
+          message:errorMsg && errorMsg.msg || '上传失败',
+          type:'error'
+
+          })
+      },
+      onRemove(){},
+      onExceed(){
+        this.$message({
+          message: '每次只能上传一本电子书',
+          type: 'warning'
+        })
+      },
     }
 }
 </script>
